b0abb3d30e3807344f31c1a985d2ea337e774a63
sonu kumar <sonu kumar@DESKTOP-B1RTCK0>
2019-08-01 10:13:05 +0530

    second commit

---
+++ Firstatt/src/main/java/com/example/demo/LoginPojo.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+public class LoginPojo {
+	private String login1;
+	private String login2;
+	public String getLogin1() {
+		return login1;
+	}
+	public void setLogin1(String login1) {
+		this.login1 = login1;
+	}
+	public String getLogin2() {
+		return login2;
+	}
+	public void setLogin2(String login2) {
+		this.login2 = login2;
+	}
+
+}
